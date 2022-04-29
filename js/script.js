aeab27317ed8c492d0c66e9f6a4ee71fb6e951c9
yuraaaaaa <wsubota@gmail.com>
2022-04-29 14:52:34 +0300

    list worked

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let input = document.querySelector('.holder__input');
+let btn   = document.querySelector('.holder__btn');
+let list  = document.querySelector('.list');
+
+btn.addEventListener('click', addListItem);
+
+function addListItem() {
+    let listItem = document.createElement('div');
+    listItem.className = 'list__item';
+    let task = document.createElement('p');
+    task.className = 'list__task'
+    task.textContent = input.value;
+    listItem.append(task);
+    list.append(listItem);
+    input.value = ''
+}
