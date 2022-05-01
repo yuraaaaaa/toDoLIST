27f6fed48eaa823271dd02e739d13106209f6a48
yuraaaaaa <wsubota@gmail.com>
2022-05-01 15:24:15 +0300

    diffirence is visible on the commits

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let list  = document.querySelector('.list');
 
 btn.addEventListener('click', addListItem);
 
+
 function addListItem() {
     let listItem = document.createElement('div');
     listItem.className = 'list__item';
@@ -14,5 +15,22 @@ function addListItem() {
     
     listItem.append(task);
     list.prepend(listItem);
-    input.value = ''
+    input.value = '';
+
+    listItem.addEventListener('mouseover', function() {
+        let delBtn = document.createElement('button');
+        delBtn.textContent = 'del';
+        listItem.append(delBtn)
+        console.log('worked')
+    
+        delBtn.addEventListener('click', function() {
+            listItem.remove()
+        })
+    });
+
+    listItem.removeEventListener('mouseout', function() {
+        delBtn.remove();
+    })
+
 }
+
